5e58b4b322277799feb4e6f017b73927c8aeeff4
fersilverio <ssilveriofernando45@gmail.com>
2021-10-06 22:12:08 -0300

    Iniciando o ast

--- ast/ast.cpp
+++ ast/ast.cpp
@@ -1,1 +1,25 @@
 // codigo de ast
+
+#include <string>
+
+
+class A_Programa{
+    public:
+        string id;
+        A_Bloco bloco;
+
+        A_Programa(string _id, A_Bloco _bloco){
+            id = _id
+            bloco = _bloco
+        }
+
+
+}
+
+
+class A_Bloco{
+    public:
+        A_LstDecSub secDecSub;
+        A_LstDecVar secDecVar;
+        A_CmdComp cmdComp;
+}
